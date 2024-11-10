20932e13343e398e0b4fe21fdaaaa050c8784997
grcng <viringuyen.work@gmail.com>
2024-11-10 03:23:14 -0500

    add full animation for mail

---
+++ letter.js
@@ -0,0 +1,19 @@
+const noti = document.getElementById("noti");
+
+noti.classList.add("noti-animate");
+
+if (noti.src.includes("noti_envelope.png")) {
+    noti.addEventListener("mouseenter", () => {
+        noti.classList.remove("noti-animate");
+    });
+    noti.addEventListener("mouseleave", () => {
+        noti.classList.add("noti-animate");
+    });
+}
+
+function openmail() {
+    noti.src = 'media/envelope.png';
+    noti.classList.remove('noti-animate');
+    noti.style.width = window.getComputedStyle(document.getElementById("mail-container")).width;
+    noti.replaceWith(noti.cloneNode(true)); // A quick way to clear all attached events on `noti`
+}
