39524850f34d1af9c1c78c8309bf5ed75b47b540
grcng <viringuyen.work@gmail.com>
2024-11-10 07:09:40 -0500

    add letters

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 // page 1
 
 function play() {
-    const video = document.getElementById("background-video");
-    video.play();
+    $("#background-video")[0].play();
 }
 
 // page 2
 
-const noti = document.getElementById("noti");
+const noti = $("#noti");
 
-noti.classList.add("noti-animate");
+noti.addClass("noti-animate");
 
-if (noti.src.includes("noti_envelope.png")) {
-    noti.addEventListener("mouseenter", () => {
-        noti.classList.remove("noti-animate");
+if (noti.attr("src").includes("noti_envelope.png")) { 
+    noti.on("mouseenter", function() { 
+        noti.removeClass("noti-animate"); 
     });
-    noti.addEventListener("mouseleave", () => {
-        noti.classList.add("noti-animate");
+    noti.on("mouseleave", function() { 
+        noti.addClass("noti-animate"); 
     });
 }
 
 function openmail() {
-    noti.style.display = 'none';
-    const mail = document.getElementById("opened-mail");
-    mail.style.display = "block";
+    $("#noti").hide(); 
+    $("#opened-mail").show(); 
+    $("#letter1").css("display", "flex"); 
 }
+
+let currentLetter = 1;
+
+function nextletter() {
+    $(`#letter${currentLetter}`).hide();
+    currentLetter++;
+    if ($(`#letter${currentLetter}`).length > 0) {
+        $(`#letter${currentLetter}`).show();
+    } else {
+        // Reset to the first letter or hide all if needed
+        currentLetter = 1;
+        $(`#letter${currentLetter}`).show();
+    }
+}
+
+function closeFullscreen() {
+    $(`.full-screen`).hide(); 
+    currentLetter = 1; 
+}
+
+$(document).on("keydown", function(event) {
+    if (event.key === "Escape") {
+        closeFullscreen(); 
+    }
+});
+
+$('.full-screen').on('click', function(e) {
+    if ($(e.target).closest('.letter').length === 0) { // Check if the click is outside the image
+        closeFullscreen();
+    }
+});
